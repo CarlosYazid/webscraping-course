8de9401c5e0f1f8ca02eb100663da511ad44d91f
Carlos Yazid <140143753+CarlosYazid@users.noreply.github.com>
2025-10-17 22:09:04 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
-wget -O /content/chrome-linux64.zip https://storage.googleapis.com/chrome-for-testing-public/136.0.7103.94/linux64/chrome-linux64.zip
+#!/bin/bash
+wget -q -O /content/chrome-linux64.zip https://storage.googleapis.com/chrome-for-testing-public/136.0.7103.94/linux64/chrome-linux64.zip
 unzip -q /content/chrome-linux64.zip -d /content/
 mv /content/chrome-linux64 /content/chrome
 rm -r -f /content/chrome-linux64.zip
-wget -O /content/chromedriver-linux64.zip https://storage.googleapis.com/chrome-for-testing-public/136.0.7103.94/linux64/chromedriver-linux64.zip
+wget -q -O /content/chromedriver-linux64.zip https://storage.googleapis.com/chrome-for-testing-public/136.0.7103.94/linux64/chromedriver-linux64.zip
 unzip -q /content/chromedriver-linux64.zip -d /content/
 mv /content/chromedriver-linux64/chromedriver /content/chromedriver
 rm -r -f /content/chromedriver-linux64.zip
 chmod +x /content/chromedriver
-pip install selenium webdriver-manager
+pip install -qq selenium webdriver-manager
